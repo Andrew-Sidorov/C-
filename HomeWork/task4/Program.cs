9c748f75ffa1e6230fcc467d3d8c12683097ea47
Andrew <sidorov_a_e@mail.ru>
2022-12-06 04:25:15 +0300

    задача 4 решена

---
+++ HomeWork/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b = "); 
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число c = "); 
+int c = int.Parse(Console.ReadLine());
+
+if (a > b && a > c)
+
+{
+Console.Write("max = " + a + " ");
+}
+
+if (b > c)
+
+{
+Console.Write("max = " + b + " ");
+}
+
+
+if (c > a)
+
+{
+Console.Write("max = " + c + " ");
+}
