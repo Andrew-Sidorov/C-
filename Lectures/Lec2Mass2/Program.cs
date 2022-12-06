26fe3412d0a657ef61fa07aa86a76663c6eff142
Andrew <sidorov_a_e@mail.ru>
2022-12-06 22:13:47 +0300

    добавил папку Lectures

---
+++ Lectures/Lec2Mass2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {91, 26, 23, 14, 14, 25, 66, 57, 82};
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
