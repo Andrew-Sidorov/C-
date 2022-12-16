06512f056b24efa8b551a978eace34ce74f92209
Andrew <sidorov_a_e@mail.ru>
2022-12-16 21:26:55 +0300

    Seive lec4

---
+++ Lectures/Lec4/Program.cs
@@ -0,0 +1,84 @@
+﻿// string[,] table = new string[2, 5];
+// // String.Empty
+// // table[0,0]  table[0,1]  table[0,2]  table[0,4]
+// // table[1,0]  table[1,1]  table[1,2]  table[1,4] 
+
+
+
+// table [1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+
+
+// void PrintArray(int[,] matr)
+// {
+
+// for (int i = 0; i < matr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matr.GetLength(1); j++)
+//     {
+//         Console.Write($"{matr[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+// }
+
+// void fillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matr.GetLength(1); j++)
+//     {
+//         matr[i,j] = new Random().Next(1,10); // [1; 10)
+//     }
+   
+// }
+
+// }
+// int[,] matrix = new  int[3, 4];
+
+// PrintArray(matrix);
+// fillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+int[,] pic = new int[,]
+
+void PrintImage(int[,] image)
+{
+
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        // Console.Write($"{matr[i, j]} ");
+        if (image[i,j] == 0) Console.WriteLine($" ");
+        else (image[i,j] == 0) Console.Write($"+");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillImage(int row, int col)
+{
+    if(pic[row, col] == 0)
+    {
+        pic[row,col] = 1;
+        FillImage(row-1, col);
+        FillImage(row, col-1);
+        FillImage(row+1, col);
+        FillImage(row, col+1);
+    }
+}
+PrintArray(pic);
+fillArray(13, 13);
+PrintArray(pic);
