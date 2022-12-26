dc6a22132a918594fcd69dd4721470bfa03a6a3b
Andrew <sidorov_a_e@mail.ru>
2022-12-26 19:50:38 +0300

    Seive task 41 43

---
+++ HomeWork/task50/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
+// двумерном массиве, и возвращает значение этого элемента или же указание, что такого 
+// элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1,7] -> такого числа в массиве нет ([1,7] это позиция по индексам, 1- строка и 
+// 7 -столбец)
+
+int[,] CreateArray(int a, int b)
+{
+    int[,] matrix = new int[a, b];
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            matrix[rows, columns] = new Random().Next(1, 10);
+            Console.Write($"{matrix[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+void Position(int[,] array, int position)
+{
+    if (position%10 > array.GetLength(1) || position/10 > array.GetLength(0))
+    Console.WriteLine($"\nЧисла с позицией ({position/10}*{position%10}) в массиве нет");
+    else Console.WriteLine($"\nЧисла с позицией ({position/10}*{position%10}) = {array[position/10 - 1, position%10 - 1]}");
+}
+
+Console.WriteLine("Введите размер массива M * N:  ");
+int size = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите позицию массива X * Y"  );
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Position(CreateArray(size/10, size%10), i);
+Console.WriteLine();
---
+++ Seminars/task46/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми 
+// числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue); //result[0,0] = 34;
+        }
+ 
+   }
+   return result;
+}
+
+void PrintArrey(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Vvedite kolichestvo strok massiva ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Vvedite kolichestvo stolbcov massiva ");
+int columns = int.Parse(Console.ReadLine());
+
+
+int[,] array = GetArray(rows,columns,0,10);
+PrintArrey(array); 
